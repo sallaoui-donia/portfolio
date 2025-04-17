578b1cf94669c533f557ff8c9fa311c8c2a7ef28
sallaoui-donia <sallaouidonia@yahoo.com>
2025-04-17 17:02:00 +0200

    Ready for Netlify

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Génère des fichiers HTML/CSS/JS statiques
+  images: {
+    unoptimized: true, // Désactive l'optimisation avancée (requise pour 'output: export')
+  },
+  trailingSlash: true, // Optionnel : garantit des URLs cohérentes
 };
-
-
 
 export default nextConfig;
